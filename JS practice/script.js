6f601d31174a2449fced37bbf49efe6b23f4e3b6
Raj Rana <be2020se715@gces.edu.np>
2022-06-07 06:19:22 +0545

    JS practice

---
+++ JS practice/script.js
@@ -0,0 +1,31 @@
+
+for (let i=1;i<=15;i++){
+    if(i % 2 != 0){
+        console.log("Odd"+i);
+    }
+    else{
+        console.log("Even"+i);
+    }
+}
+var y = "r" ;
+console.log(y);
+let student=["ram","sa"];
+console.log(student[0])
+
+
+//ex1
+    num1=prompt("Enter first number");
+    num2=prompt("Enter second number");
+if(num1>num2){
+    console.log("The larger number is" ,num1);
+}
+else{
+    console.log("The larger number is",num2);
+}
+const array = [1, 2, 3, 4];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+console.log(sum);
